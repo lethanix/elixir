fa8d66414046ce6399abe4514ebed1e27debc1b9
lethanix <me@lethanix.com>
2025-07-02 00:01:39 -0600

    Add Hello World

---
+++ servy/lib/servy.ex
@@ -0,0 +1,20 @@
+defmodule Servy do
+  @moduledoc """
+  Documentation for `Servy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Servy.hello(name)
+      Hello, name
+
+  """
+  def hello(name) do
+    "Hello, #{name}"
+  end
+end
+
+# IO.puts Servy.hello("Lethanix")
