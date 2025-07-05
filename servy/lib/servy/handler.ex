b20f90214ac4384cf2762c905751197b36b67f63
lethanix <me@lethanix.com>
2025-07-04 23:30:40 -0600

    Add status codes

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -16,7 +16,7 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, status: nil, resp_body: ""}
   end
 
   def route(conv) do
@@ -24,22 +24,34 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/compute") do
-    %{ conv | resp_body: "Lambda, EC2" }
+    %{ conv | status: 200, resp_body: "Lambda, EC2" }
   end
 
   def route(conv, "GET", "/messaging") do
-    %{ conv | resp_body:  "SQS, SNS, MQ "}
+    %{ conv | status: 200, resp_body:  "SQS, SNS, MQ "}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_code(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
+
+  defp status_code(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+
 end
 
 request = """
